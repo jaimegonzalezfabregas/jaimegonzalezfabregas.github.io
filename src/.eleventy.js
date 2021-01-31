2784471057f159af9ec7b798e8cb029e0509bab2
Jaime <jaimegonzalezfabregas@gmail.com>
2021-01-31 19:11:30 +0100

    new comit

--- src/.eleventy.js
+++ src/.eleventy.js
@@ -1,6 +1,8 @@
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy('css')
     eleventyConfig.addPassthroughCopy('images')
+    eleventyConfig.addPassthroughCopy('scripts')
+
     return {
         passthroughFileCopy: true
     }
